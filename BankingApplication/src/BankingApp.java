c0356c9eea1e1636830df225dbbb8844ff2ba3e5
Ali Hassan <ahassan0213@gmail.com>
2021-08-24 22:34:43 -0400

    Finished Register Fxml file, and added methods to create view

--- BankingApplication/src/BankingApp.java
+++ BankingApplication/src/BankingApp.java
@@ -6,22 +6,43 @@ import javafx.stage.Stage;
 
 public class BankingApp extends Application {
 
-	Stage stage;
+	private Stage stage;
+	private Scene login, main, register;
+	
 	public void makeMainScreen() {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-			Scene scene = new Scene(root, 225, 250);
-			stage.setScene(scene);
+			main = new Scene(root, 225, 250);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	public void makeLoginScreen() {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+			 login = new Scene(root, 225, 250);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void makeRegisterScreen() {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("register.fxml"));
+			 register = new Scene(root, 450, 570);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	@Override
 	public void start(Stage stage) throws Exception {
 		this.stage = stage;
 		makeMainScreen();
+		makeLoginScreen();
+		makeRegisterScreen();
+		stage.setScene(register);
 		stage.setResizable (false);
 		stage.setTitle("No Name Banking");
 		stage.show();
