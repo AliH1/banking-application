0094f6b284c817a2662d42452f0be953115c4922
Ali Hassan <ahassan0213@gmail.com>
2021-08-27 10:18:36 -0400

    added css file for styles, got rid of unnecessary scene, added scene
    switching to controller and connect controller to fxml files

--- BankingApplication/src/BankingApp.java
+++ BankingApplication/src/BankingApp.java
@@ -2,49 +2,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class BankingApp extends Application {
 
-	private Stage stage;
-	private Scene login, main, register;
-	
-	public void makeMainScreen() {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-			main = new Scene(root, 225, 250);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void makeLoginScreen() {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-			 login = new Scene(root, 225, 250);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void makeRegisterScreen() {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("register.fxml"));
-			 register = new Scene(root, 450, 570);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
 	@Override
 	public void start(Stage stage) throws Exception {
-		this.stage = stage;
-		makeMainScreen();
-		makeLoginScreen();
-		makeRegisterScreen();
-		stage.setScene(register);
+		Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add("application.css");
+		stage.setScene(scene);
 		stage.setResizable (false);
-		stage.setTitle("No Name Banking");
+		stage.centerOnScreen();
+		Image icon = new Image("icon.png");
+		stage.getIcons().add(icon);
+		stage.setTitle("Banking App");
 		stage.show();
 		
 	}
--- BankingApplication/src/BankingAppController.java
+++ BankingApplication/src/BankingAppController.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -5,8 +6,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class BankingAppController {
-	Connection connection;
+	
+	private Connection connection;
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
 	
 	public void connectToDB() {
 	      try {
@@ -31,6 +43,39 @@ public class BankingAppController {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	public void loginPressed(ActionEvent e) {
+		
+	}
+	
+	public void registerPressed(ActionEvent e) throws IOException {
+		switchToLoginScene(e);
+	}
+	
+	public void signUpPressed(ActionEvent e) throws IOException {
+		switchToRegisterScene(e);
+	}
+	
+	public void backPressed(ActionEvent e) throws IOException {
+		switchToLoginScene(e);
+	}	
+	
+	public void switchToLoginScene(ActionEvent e) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("login.fxml"));
+		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		scene.getStylesheets().add("application.css");
+		stage.setScene(scene);
+		stage.centerOnScreen();
+	}
+	
+	public void switchToRegisterScene(ActionEvent e) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("register.fxml"));
+		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		scene.getStylesheets().add("application.css");
+		stage.setScene(scene);
+		stage.centerOnScreen();
 	}
 	
 	public boolean checkLoginDetails(String username, String password) throws SQLException {
